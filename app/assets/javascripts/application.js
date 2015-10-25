c2c8e6aec6ffe67cfed7bd1a624162bd13fe352f
Ravikanthbk <ravikanthnawada@gmail.com>
2015-10-25 20:42:15 +0800

    Events calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
@@ -21,44 +22,79 @@
 //= require pickers
 //= require fullcalendar
 
-var updateEvent;
-
 $(document).ready(function() {
-  return $('#calendar').fullCalendar({
-    editable: true,
-    header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,agendaWeek,agendaDay'
-    },
-    defaultView: 'month',
-    height: 500,
-    slotMinutes: 30,
-    eventSources: [
-      {
-        url: '/events'
-      }
-    ],
-    timeFormat: 'h:mm t{ - h:mm t} ',
-    dragOpacity: "0.5",
-    eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc) {
-      return updateEvent(event);
-    },
-    eventResize: function(event, dayDelta, minuteDelta, revertFunc) {
-      return updateEvent(event);
-    }
-  });
+//  return $('#calendar').fullCalendar();
+
+$(function() {
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		defaultDate: '2015-10-12',
+		editable: true,
+		eventLimit: true, // allow "more" link when too many events
+		events: [
+			{
+				title: 'All Day Event',
+				start: '2015-10-01'
+			},
+			{
+				title: 'Long Event',
+				start: '2015-10-07',
+				end: '2015-10-10'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2015-10-09T16:00:00'
+			},
+			{
+				id: 999,
+				title: 'Repeating Event',
+				start: '2015-10-16T16:00:00'
+			},
+			{
+				title: 'Conference',
+				start: '2015-10-11',
+				end: '2015-10-13'
+			},
+			{
+				title: 'Meeting',
+				start: '2015-10-12T10:30:00',
+				end: '2015-10-12T12:30:00'
+			},
+			{
+				title: 'Lunch',
+				start: '2015-02-12T12:00:00'
+			},
+			{
+				title: 'Meeting',
+				start: '2015-10-12T14:30:00'
+			},
+			{
+				title: 'Happy Hour',
+				start: '2015-10-12T17:30:00'
+			},
+			{
+				title: 'Dinner',
+				start: '2015-10-12T20:00:00'
+			},
+			{
+				title: 'Birthday Party',
+				start: '2015-10-13T07:00:00'
+			},
+			{
+				title: 'Click for Google',
+				url: 'http://google.com/',
+				start: '2015-10-28'
+			}
+		]
+	});
 });
 
-updateEvent = function(the_event) {
-  return $.update("/events/" + the_event.id, {
-    event: {
-      title: the_event.title,
-      starts_at: "" + the_event.start,
-      ends_at: "" + the_event.end,
-      description: the_event.description
-    }
-  });
-};
+
+});
 
 
